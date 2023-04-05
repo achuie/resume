59fdeae2814d1b086fd654e733105875af44113f
achuie <31836492+achuie@users.noreply.github.com>
2023-04-06 00:59:08 +0900

    Nixify (#1)
    * Skeleton of flake.
    
    * Flake works; update gitignore for Nix artifacts.
    
    * Genericize derivation for cover letter.
    
    * Add app to compile cover letter with parameterized date.
    
    * Cleanup; ignore .cache.
    
    * No need for a makefile anymore.

---
+++ flake.nix
@@ -0,0 +1,63 @@
+{
+  description = "Build requirements for resume";
+
+  inputs.nixpkgs.url = "nixpkgs/22.11";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        tex = pkgs.texlive.combine {
+          inherit (pkgs.texlive)
+            scheme-small latex-bin latexmk bold-extra titlesec titling
+            changepage datetime2 tracklang;
+        };
+        mkTeXDrvForDoc = doc: prettyName:
+          (pkgs.stdenvNoCC.mkDerivation {
+            name = prettyName;
+
+            src = self;
+            buildPhase = ''
+              export PATH="${pkgs.lib.makeBinPath [ pkgs.coreutils tex ]}";
+              mkdir -p .cache/texmf-var
+              env TEXMFHOME=.cache TEXMFVAR=.cache/texmf-var \
+                latexmk -interaction=nonstopmode -pdf -lualatex \
+                ${doc}.tex
+            '';
+            installPhase = ''
+              mkdir $out
+              cp ${doc}.pdf $out/${prettyName}.pdf
+            '';
+            fixupPhase = "true";
+          });
+
+        coverLetterDateScript = pkgs.writeShellScriptBin "compileCoverLetterWithDate.sh" ''
+          export PATH="${pkgs.lib.makeBinPath [ pkgs.coreutils tex ]}";
+          mkdir -p .cache/texmf-var
+          env SOURCE_DATE_EPOCH=$(date -d "$1" +%s) LC_ALL=C LANG=en_US \
+            TEXMFHOME=.cache TEXMFVAR=.cache/texmf-var \
+            latexmk -interaction=nonstopmode -pdf -lualatex -gg \
+            cletter.tex
+
+          cp cletter.pdf cover_letter.pdf
+          latexmk -C cletter.tex
+        '';
+      in rec {
+        packages = rec {
+          resume = mkTeXDrvForDoc "resume" "andrew_huie";
+          cover-letter = mkTeXDrvForDoc "cletter" "cover_letter";
+
+          default = resume;
+        };
+
+        apps = {
+          cover-letter = {
+            type = "app";
+            program = "${coverLetterDateScript}/bin/compileCoverLetterWithDate.sh";
+          };
+        };
+
+        formatter = pkgs.nixfmt;
+      });
+}
