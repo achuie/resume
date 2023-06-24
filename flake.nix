d903a601792404b0dce1d96234a08cdb41f746c2
achuie <achuie@protonmail.com>
2023-06-24 21:26:43 +0900

    Compose flake packages with nixpkgs.

--- flake.nix
+++ flake.nix
@@ -1,62 +1,65 @@
 {
   description = "Build requirements for resume";
 
-  inputs.nixpkgs.url = "nixpkgs/22.11";
-  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-        tex = pkgs.texlive.combine {
-          inherit (pkgs.texlive) scheme-small latex-bin latexmk bold-extra titlesec titling
-            changepage datetime2 tracklang;
-        };
-        mkTeXDrvForDoc = doc: prettyName:
-          (pkgs.stdenvNoCC.mkDerivation {
-            name = prettyName;
-
-            src = self;
-            buildPhase = ''
-              export PATH="${pkgs.lib.makeBinPath [ pkgs.coreutils tex ]}";
-              mkdir -p .cache/texmf-var
-              env TEXMFHOME=.cache TEXMFVAR=.cache/texmf-var \
-                latexmk -interaction=nonstopmode -pdf -lualatex \
-                ${doc}.tex
-            '';
-            installPhase = ''
-              mkdir $out
-              cp ${doc}.pdf $out/${prettyName}.pdf
-            '';
-            fixupPhase = "true";
-          });
-
-        coverLetterDateScript = pkgs.writeShellScriptBin "compileCoverLetterWithDate.sh" ''
-          export PATH="${pkgs.lib.makeBinPath [ pkgs.coreutils tex ]}";
-          mkdir -p .cache/texmf-var
-          env SOURCE_DATE_EPOCH=$(date -d "$1" +%s) LC_ALL=C LANG=en_US \
-            TEXMFHOME=.cache TEXMFVAR=.cache/texmf-var \
-            latexmk -interaction=nonstopmode -pdf -lualatex -gg \
-            cletter.tex
-
-          cp cletter.pdf cover_letter.pdf
-          latexmk -C cletter.tex
-        '';
-      in
-      {
-        packages = rec {
+  outputs = { self, nixpkgs }:
+    let
+      forAllSystems = f: nixpkgs.lib.genAttrs [ "x86_64-linux" ] (system: f
+        { pkgs = nixpkgs.legacyPackages.${system}; this = self.packages.${system}; });
+    in
+    {
+      packages = forAllSystems (pset:
+        let
+          mkTeXDrvForDoc = doc: prettyName:
+            (pset.pkgs.stdenvNoCC.mkDerivation {
+              name = prettyName;
+              src = self;
+              buildPhase = ''
+                export PATH="${pset.pkgs.lib.makeBinPath [ pset.pkgs.coreutils pset.this.tex ]}";
+                mkdir -p .cache/texmf-var
+                env TEXMFHOME=.cache TEXMFVAR=.cache/texmf-var \
+                  latexmk -interaction=nonstopmode -pdf -lualatex \
+                  ${doc}.tex
+              '';
+              installPhase = ''
+                mkdir $out
+                cp ${doc}.pdf $out/${prettyName}.pdf
+              '';
+              fixupPhase = "true";
+            });
+        in
+        {
+          tex = pset.pkgs.texlive.combine {
+            inherit (pset.pkgs.texlive) scheme-small latex-bin latexmk bold-extra titlesec titling
+              changepage datetime2 tracklang;
+          };
           resume = mkTeXDrvForDoc "resume" "andrew_huie";
           cover-letter = mkTeXDrvForDoc "cletter" "cover_letter";
-          default = resume;
-        };
+          default = pset.this.resume;
+        });
 
-        apps = {
+      apps = forAllSystems (pset:
+        let
+          coverLetterDateScript = pset.pkgs.writeShellScriptBin "compileCoverLetterWithDate.sh" ''
+            export PATH="${pset.pkgs.lib.makeBinPath [ pset.pkgs.coreutils pset.this.tex ]}";
+            mkdir -p .cache/texmf-var
+            env SOURCE_DATE_EPOCH=$(date -d "$1" +%s) LC_ALL=C LANG=en_US \
+              TEXMFHOME=.cache TEXMFVAR=.cache/texmf-var \
+              latexmk -interaction=nonstopmode -pdf -lualatex -gg \
+              cletter.tex
+
+            cp cletter.pdf cover_letter.pdf
+            latexmk -C cletter.tex
+          '';
+        in
+        {
           cover-letter = {
             type = "app";
             program = "${coverLetterDateScript}/bin/compileCoverLetterWithDate.sh";
           };
-        };
+        });
 
-        formatter = pkgs.nixpkgs-fmt;
-      });
+      formatter = forAllSystems (pset: pset.pkgs.nixpkgs-fmt);
+    };
 }
